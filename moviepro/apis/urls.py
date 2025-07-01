3955d8ae23a4ec875c7766cea79bdf1055189f64
Elvin <elvinhaxverdiyev777@gamil.com>
2025-07-01 23:02:13 +0400

    create new files for apis"

--- moviepro/apis/urls.py
+++ moviepro/apis/urls.py
@@ -1,19 +1,75 @@
 from django.urls import path
+from .auth_apis import *
 from .views import *
 
 urlpatterns = [
-    path('register/', UserRegisterAPIView.as_view(), name="register"),
-    path('login/', LogInAPIView.as_view(), name="login"),
-    path('account/', AccountDetailAPIView.as_view(), name="account-detail"),
-    path('account/change-password/', ChangePasswordAPIView.as_view(), name="change-password"),  
-    path('logout/', UserLogoutAPIView.as_view(), name="logout"),
-    path('users/', UserListAPIView.as_view(), name="user-list"),
-    path('movies/', MovieListPostAPIView.as_view(), name="movie-list"),
-    path('movies/<int:id>/', MovieDetailAPIView.as_view(), name="movie-detail"),
-    path('movies/<int:id>/like/', MovieLikeAPIView.as_view(), name="movie-like"),
-    path('movies/<int:id>/comments/', AddCommentAPIView.as_view(), name="add-comment"),
-    path('comments/<int:id>/delete/', DeleteCommentAPIView.as_view(), name="delete-comment"),
-    path('movies/search/', MovieSearchAPIView.as_view(), name="movie-search"),
-    path('movies/by_genre/', MovieByGenreAPIView.as_view(), name="movies-by-genre"),
-    path('genres/', GenreListAPIView.as_view(), name="genre-list")
+    path(
+        'register/',
+        UserRegisterAPIView.as_view(),
+        name="register"
+    ),
+    path(
+        'login/',
+        LogInAPIView.as_view(),
+        name="login"
+    ),
+    path(
+        'account/',
+        AccountDetailAPIView.as_view(),
+        name="account-detail"
+    ),
+    path(
+        'account/change-password/',
+        ChangePasswordAPIView.as_view(),
+        name="change-password"
+    ),  
+    path('logout/',
+         UserLogoutAPIView.as_view(),
+         name="logout"
+    ),
+    path(
+        'users/',
+        UserListAPIView.as_view(),
+        name="user-list"
+    ),
+    path(
+        'movies/',
+        MovieListPostAPIView.as_view(),
+        name="movie-list"
+    ),
+    path(
+        'movies/<int:id>/',
+        MovieDetailAPIView.as_view(),
+        name="movie-detail"
+    ),
+    path(
+        'movies/<int:id>/like/',
+        MovieLikeAPIView.as_view(), 
+        name="movie-like"
+    ),
+    path(
+        'movies/<int:id>/comments/',
+        AddCommentAPIView.as_view(),
+        name="add-comment"
+    ),
+    path(
+        'comments/<int:id>/delete/',
+        DeleteCommentAPIView.as_view(),
+        name="delete-comment"
+    ),
+    path(
+        'movies/search/',
+        MovieSearchAPIView.as_view(),
+        name="movie-search"
+    ),
+    path(
+        'movies/by_genre/',
+        MovieByGenreAPIView.as_view(),
+        name="movies-by-genre"
+    ),
+    path(
+        'genres/',
+        GenreListAPIView.as_view(),
+        name="genre-list"
+    )
 ]
