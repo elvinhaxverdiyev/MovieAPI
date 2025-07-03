ae546c95ec8f42b00e9bf62fcc2f96fe88a5d8f9
Elvin <elvinhaxverdiyev777@gamil.com>
2025-07-03 17:15:23 +0400

    added swagger for auth apis

--- moviepro/users/serializers.py
+++ moviepro/users/serializers.py
@@ -2,13 +2,30 @@ from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth import authenticate
 
-
 from .models import CustomUser
+
+
+from rest_framework import serializers
+from .models import CustomUser
+
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        exclude = ["password"]
+        fields = (
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "bio",
+            "image",
+        )
+        extra_kwargs = {
+            "email": {"required": False, "allow_blank": True},
+            "bio": {"required": False, "allow_blank": True},
+            "image": {"required": False},
+        }
         
         
 class ChangePasswordSerializer(serializers.Serializer):
@@ -16,10 +33,17 @@ class ChangePasswordSerializer(serializers.Serializer):
     new_password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     confirm_password = serializers.CharField(write_only=True, required=True)
 
-    def validate(self, data):
-        if data["new_password"] != data["confirm_password"]:
-            raise serializers.ValidationError({"confirm_password": "Passwords must match"})
-        return data
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError("Old password is incorrect")
+        return value
+
+    def save(self, **kwargs):
+        user = self.context['request'].user
+        user.set_password(self.validated_data['new_password'])
+        user.save()
+        return user
 
 
 class CustomUserRegisterSerializer(serializers.ModelSerializer):
@@ -28,28 +52,26 @@ class CustomUserRegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = "__all__"
+        fields = ("username", "email", "password", "password_two", "first_name", "last_name", "bio", "image")
 
     def validate(self, data):
         if data["password"] != data["password_two"]:
             raise serializers.ValidationError("Passwords must match")
         return data
-            
+
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            username=validated_data["username"],
-            email=validated_data["email"],
-            password=validated_data["password"]
-        )
-        return user
+        validated_data.pop("password_two")
+        return CustomUser.objects.create_user(**validated_data)
+
     
     
 class LoginSerializer(serializers.Serializer):
-    user_name = serializers.CharField(max_length=150)
+    username = serializers.CharField(max_length=150)
     password = serializers.CharField(write_only=True)
-    
+
     def validate(self, data):
-        user = authenticate(username=data["user_name"], password=data["password"])
+        user = authenticate(username=data["username"], password=data["password"])
         if user:
             return {"user": user}
         raise serializers.ValidationError("Invalid username or password")
+
