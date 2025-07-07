979cc627e96369afb432f2a1b6c31614f3cd50f6
Elvin <elvinhaxverdiyev777@gamil.com>
2025-07-07 18:56:11 +0400

    add dog string for user model

--- moviepro/users/models/user_models.py
+++ moviepro/users/models/user_models.py
@@ -3,6 +3,16 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
+    """
+    Custom user model that extends Django's built-in AbstractUser.
+
+    Additional Fields:
+    - email (EmailField): Unique email address. Can be null or blank.
+    - bio (TextField): A short biography or description of the user (max 550 characters).
+    - image (ImageField): Optional profile image uploaded to 'images/' directory.
+
+    Inherits all fields and functionality from AbstractUser (e.g., username, password, etc.).
+    """
     email = models.EmailField(unique=True, null=True, blank=True)
     bio = models.TextField(max_length=550, null=True, blank=True)
     image = models.ImageField(upload_to="images/", null=True, blank=True)
