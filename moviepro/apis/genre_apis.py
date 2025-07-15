512427c410db1e9d54c81c8f67b77ff395a9fd16
Elvin <elvinhaxverdiyev777@gamil.com>
2025-07-15 22:38:40 +0400

    create redis configure and cached genres

--- moviepro/apis/genre_apis.py
+++ moviepro/apis/genre_apis.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.core.cache import cache
+import logging
 
 from movieapp.models import Category
 from movieapp.serializers import CategorySerializer
@@ -13,16 +15,26 @@ __all__ = [
     "MovieByGenreAPIView"
 ]
 
+logger = logging.getLogger(__name__)
+
 class GenreListAPIView(APIView):
-    
     def get(self, request):
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        categories = cache.get('categories')
+        logger.info(f"Cachedən categories: {categories}")
+
+        if not categories:
+            categories_qs = Category.objects.all()
+            serializer = CategorySerializer(categories_qs, many=True)
+            categories = serializer.data
+            cache.set('categories', categories, timeout=31536000)
+            logger.info("Cache yeniləndi")
+        else:
+            logger.info("Cache istifadə edildi")
+
+        return Response(categories, status=status.HTTP_200_OK)
     
-    
+            
 class MovieByGenreAPIView(APIView):
-    """API that lists movies based on the specified genre."""
     permission_classes = [IsAuthenticatedOrReadOnly]
     
     def get(self, request):
@@ -35,11 +47,6 @@ class MovieByGenreAPIView(APIView):
                 serializer = MovieSerializer(movies, many=True) 
                 return Response(serializer.data, status=status.HTTP_200_OK)
             except Category.DoesNotExist:
-                
-                return Response({"error": "Genre not found"}, 
-                                status=status.HTTP_404_NOT_FOUND
-                                )
+                return Response({"error": "Genre not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        return Response({"error": "Genre parameter is required"},
-                        status=status.HTTP_400_BAD_REQUEST
-                        )
+        return Response({"error": "Genre parameter is required"}, status=status.HTTP_400_BAD_REQUEST)
