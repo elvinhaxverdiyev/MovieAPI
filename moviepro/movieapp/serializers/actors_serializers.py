162deff532b85622545cc308e56d2269f5949ccd
Elvin <elvinhaxverdiyev777@gamil.com>
2025-08-24 17:32:23 +0400

    added dog string for actor serializer

--- moviepro/movieapp/serializers/actors_serializers.py
+++ moviepro/movieapp/serializers/actors_serializers.py
@@ -4,6 +4,13 @@ from movieapp.models.actors_models import Actor
 
 
 class ActorsSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Actor model.
+
+    This serializer includes all fields of the Actor model (`__all__`) 
+    and is mainly used for creating, retrieving, updating, 
+    and deleting actor instances (CRUD operations).
+    """
     class Meta:
         model = Actor
         fields = "__all__"
