c852426fce2201405d4196cf882d00a4653a851d
Elvin <elvinhaxverdiyev777@gamil.com>
2025-08-24 17:35:26 +0400

    added dog string for comment serializer

--- moviepro/movieapp/serializers/interactions_serializers.py
+++ moviepro/movieapp/serializers/interactions_serializers.py
@@ -4,6 +4,13 @@ from movieapp.models.interactions_models import Comment, MovieLike
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Comment model.
+
+    This serializer converts Comment instances to and from JSON format.
+    It includes all fields from the Comment model, but represents the
+    related user using its `__str__` method via StringRelatedField.
+    """
     user = serializers.StringRelatedField()
     
     class Meta:
